e6538b30a5eaa80a5dcc44cc5bf6efa9931a2966
yu-zyu <siragaigarasi@gmail.com>
2021-09-20 00:23:56 +0900

    パターン変数（変数名が大文字から始まる）を実装した

---
+++ chap6/program05/Example6.scala
@@ -0,0 +1,14 @@
+package chap06.program04
+
+object Example5 {
+  def main(args: Array[String]):Unit = {
+    val colors = List("sky", "cloud", "car")
+    for (color <- colors) {
+      color match {
+        case "sky" => println("blue")
+        case "cloud" => println("gray")
+        case Ambiguous => println(ambiguous + " is too ambiguous.")
+      }
+    }
+  }
+}
